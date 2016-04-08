a10517ef837ae6a9c9d3d0073d600018866da56e
Brian M. Waters <brian@brianmwaters.net>
2016-04-08 08:05:51 -0400

    Add BoardException class

---
+++ src/main/java/edu/uvm/teamawesome/sorry/models/BoardException.java
@@ -0,0 +1,20 @@
+package edu.uvm.teamawesome.sorry.models;
+
+/**
+ * Thrown to indicate that a method has requested an illegal board
+ * configuration.
+ * <p>
+ * Note that this is not intended to indicate board configurations that are
+ * illegal according to the rules of Sorry. Instead, it indicates board
+ * configurations that are physically or structurally unsound, such as two
+ * pieces occupying the same square (single-piece) space.
+ * <p>
+ * @author <a href="mailto:brian@brianmwaters.net">Brian M. Waters</a>
+ */
+public final class BoardException extends Exception {
+    /**
+     * Constructs a BoardException.
+     */
+    public BoardException() {
+    }
+}
